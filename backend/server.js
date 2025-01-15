a32c12680712783d2cc93795fa82676b7f67aaff
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-01-16 01:35:37 +0530

    feat: add new CORS origin for data sending endpoint

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'https://divine-pellets.vercel.app',
+    'https://divine-pellets.vercel.app/send-data',
   ], // Replace with your frontend URL
   methods: 'GET,HEAD,POST', // Allowed HTTP methods
   credentials: true, // Allow cookies if needed
